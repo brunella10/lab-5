ebd3bc55b9a1e2a9e26f71424235153cab3a471a
Gabriela Diez <gaby1010@gmail.com>
2017-06-10 19:14:38 -0700

    second problem finished

--- app.js
+++ app.js
@@ -27,12 +27,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function multiply(a,b){ //eslint-disable-line
-
+function multiply(a,b) { //eslint-disable-line
+  var arg1 = a * b;
+  var text = 'The product of ' + a + ' and ' + b + ' is ' + arg1 + '.';
+  console.log(text);
+  return [arg1, text];
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
